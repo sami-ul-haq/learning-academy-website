a8dcddbb297d1167d6f554009d00fec3938f33f4
Sami ul Haq <samiulhaq1899@gmail.com>
2020-08-08 21:13:11 +0500

    not responsive

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let menuBtn = document.getElementById('menuBtn');
+let sideNav = document.getElementById('sideNav');
+
+sideNav.style.right = "-250px";
+menuBtn.onclick = function(){
+    if (sideNav.style.right == "-250px") {
+        sideNav.style.right = "0";
+    }
+    else{
+        sideNav.style.right = "-250px";
+    }
+}
